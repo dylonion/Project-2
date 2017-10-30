8aecc289583ab758347cccdaf26b49a8a3748002
Dylan Moylan <dylanmoylan1@gmail.com>
2017-10-30 10:23:48 -0400

    Adds default Auth code, reset.css, and various other boilerplate files.

--- db/config.js
+++ db/config.js
@@ -5,7 +5,7 @@ let db;
 if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
   db = pgp({
   // custom database name
-  database: 'todolist_tc',
+  database: 'parkinglist_tc',
   port: 5432,
   host: 'localhost',
   });
